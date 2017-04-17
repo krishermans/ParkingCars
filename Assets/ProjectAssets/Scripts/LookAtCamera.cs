e2e954122ea3db71a4a018ad8f95eaa3faa54f13
Kris Hermans <Kris.Hermans@live.be>
2017-04-17 20:42:00 +0200

    created CarHolder prefab

---
+++ Assets/ProjectAssets/Scripts/LookAtCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.LookAt(Camera.main.transform);
+        gameObject.transform.Rotate(Vector3.up, 180f);	
+	}
+}
