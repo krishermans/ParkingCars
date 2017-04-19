2639f834fbab4751ce6390b15a1365334d8f796b
Kris Hermans <Kris.Hermans@live.be>
2017-04-19 22:18:43 +0200

    Added Place Cars Speech Command

---
+++ Assets/ParkingSpaceScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkingSpaceScript : MonoBehaviour
+{
+    public GameObject Car;
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlaceCars()
+    {
+        Debug.Log("Place the cars");
+        CreateCar();
+    }
+
+    private void CreateCar()
+    {
+        var c = Instantiate(Car);
+    }
+}
---
+++ Assets/ProjectAssets/Scripts/CarLoader.cs
@@ -0,0 +1,24 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CarLoader : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputEventData eventData)
+    {
+        Debug.Log("Create the car");
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
